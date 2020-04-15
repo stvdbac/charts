bda070d6288695dc2e137d75ad542d2e68860bd5
Steve De Backer <steve.debacker@icometrix.com>
2020-04-15 10:07:51 +0200

    handle null starting points

--- charts_flutter/lib/src/util/monotonex.dart
+++ charts_flutter/lib/src/util/monotonex.dart
@@ -8,12 +8,20 @@ class MonotoneX {
 
   // Calculate a one-sided slope.
   static double _slope2(double x0, double y0, double x1, double y1, double t) {
+    if (x0 == null || y0 == null) {
+      return t;
+    }
     var h = x1 - x0;
     return h != 0 ? (3 * (y1 - y0) / h - t) / 2 : t;
   }
 
   static double _slope3(
       double x0, double y0, double x1, double y1, double x2, double y2) {
+    if (x0 == null || y0 == null) {
+      // x0 = x1;
+      // y0 = y1;
+      return 0;
+    }
     double h0 = x1 - x0;
     double h1 = x2 - x1;
     double s0 = (y1 - y0) /
@@ -31,6 +39,10 @@ class MonotoneX {
   // with respect to the four values p0, p0 + m0 / 3, p1 - m1 / 3, p1".
   static Path _point(Path path, double x0, double y0, double x1, double y1,
       double t0, double t1) {
+    if (x0 == null || y0 == null) {
+      x0 = x1;
+      y0 = y1;
+    }
     var dx = (x1 - x0) / 3;
     path.cubicTo(x0 + dx, y0 + dx * t0, x1 - dx, y1 - dx * t1, x1, y1);
     return path;
@@ -71,11 +83,11 @@ class MonotoneX {
       t0 = t1;
     }
     if (reversed) {
-      arr.reversed.forEach((f){
+      arr.reversed.forEach((f) {
         _point(path, f[2], f[3], f[0], f[1], f[5], f[4]);
       });
     } else {
-      arr.forEach((f){
+      arr.forEach((f) {
         _point(path, f[0], f[1], f[2], f[3], f[4], f[5]);
       });
     }
